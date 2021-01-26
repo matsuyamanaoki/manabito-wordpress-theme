df82e45d98160b42d9dab927b1274dba931b9c96
matsuyamanaoki <mountain.30hope@docomo.ne.jp>
2021-01-26 20:55:10 +0900

    テーマの基本ファイル作成

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer>
+	<address>
+		山梨県富士吉田市X-X-X<br class="sp-only">
+		電話 0120-XXX-XXX<br class="sp-only">
+		営業時間 9:00〜17:00（休園日：月曜日、年末年始）
+	</address>
+	<span class="copyright"><small>&copy; 2014 ZOO LOGICAL All rights reserved.</small></span>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
