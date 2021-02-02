bfa941e74647336c90c8a907f65aebebf8e49c8d
matsuyamanaoki <mountain.30hope@docomo.ne.jp>
2021-02-02 21:12:32 +0900

    読み込みの設定

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+function theme_setup() {
+	add_theme_support( 'title-tag' );
+	add_theme_support( 'custom-logo' );
+}
+add_action( 'after_setup_theme', 'theme_setup' );
+
+function theme_styles() {
+	wp_enqueue_style( 'theme-reset', get_template_directory_uri() . '/css/normalize.css', array(), null );
+	wp_enqueue_style( 'bxslider-style', 'https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css', array( 'theme-reset' ), null );
+	wp_enqueue_style( 'theme-style', get_template_directory_uri() . '/css/style.css', array( 'bxslider-style' ), null );
+}
+add_action( 'wp_enqueue_scripts', 'theme_styles' );
+
+function theme_scripts() {
+	wp_deregister_script( 'jquery' );
+	wp_deregister_script( 'jquery-migrate' );
+
+	wp_enqueue_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js', array(), null, true );
+	wp_enqueue_script( 'bxslider', 'https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js', array( 'jquery' ), null, true );
+	wp_enqueue_script( 'theme_pagetop', get_template_directory_uri() . '/js/pagetop.js', array( 'jquery' ), null, true );
+	wp_enqueue_script( 'theme_slider', get_template_directory_uri() . '/js/slider.js', array( 'bxslider' ), null, true );
+}
+add_action( 'wp_enqueue_scripts', 'theme_scripts' );
--- header.php
+++ header.php
@@ -1,23 +1,22 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html <?php language_attributes(); ?>>
 <head>
-	<meta charset="utf-8">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
-	<title>ZOO LOGICAL</title>
-	<link rel="stylesheet" href="css/normalize.css">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
-	<link rel="stylesheet" href="css/style.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
-	<script src="js/slider.js"></script>
-	<script src="js/pagetop.js"></script>
 	<?php wp_head(); ?>
 </head>
 <body>
 	<header>
 		<div class="header-logo">
 			<a href="index.html">
-				<img src="images/logo.png" alt="ZOO LOGICAL">
+				<?php
+				if ( has_custom_logo() ) {
+					$custom_logo_id = get_theme_mod( 'custom_logo' );
+					$image          = wp_get_attachment_image_src( $custom_logo_id, 'full' );
+					$html           = '<img src="' . $image[0] . '" alt="' . esc_attr( get_bloginfo( 'name' ) ) . '">';
+					echo $html;
+				}
+				?>
 			</a>
 		</div>
 		<nav class="nav-global">
