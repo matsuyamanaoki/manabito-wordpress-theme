fce88297336872df5bd48c8e3b080e070cedb2f1
matsuyamanaoki <mountain.30hope@docomo.ne.jp>
2021-02-23 21:11:10 +0900

    固定ページ中身

--- page.php
+++ page.php
@@ -1,6 +1,24 @@
 <?php get_header(); ?>
+
+<?php
+if ( shortcode_exists( 'wp-structuring-markup-breadcrumb' ) ) {
+	echo '<nav class="breadcrumb">';
+	echo do_shortcode( '[wp-structuring-markup-breadcrumb]' );
+	echo '</nav>';
+}
+?>
 <div class="l-contents-wrapper">
-	<main></main>
+	<main>
+		<?php
+		if ( have_posts() ) :
+			while ( have_posts() ) :
+				the_post();
+				the_title( '<h1>', '</h1>' );
+				the_content();
+			endwhile;
+		endif;
+		?>
+	</main>
 	<?php get_sidebar(); ?>
 	<nav id="page_top" class="page-top">ページの先頭へ戻る</nav>
 </div>
